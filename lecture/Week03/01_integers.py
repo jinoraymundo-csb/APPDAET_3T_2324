26bdb4b9dc65d7be818402d18b85de74b2f622f7
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-05-22 19:18:32 +0800

    added lecture Week3

---
+++ lecture/Week03/01_integers.py
@@ -0,0 +1,23 @@
+class_size = 9
+students_present = 5.1
+student_surnames = [
+    "Abad",
+    "Hermoza",
+    "Gaza",
+    "Malanog",
+    "Vinson",
+]
+appdaet = {
+    "title": "Application Development and Emerging Technologies",
+    "prerequisite_to": "APPINTR"
+}
+
+print(type(class_size))
+print(type(students_present))
+print(type(student_surnames))
+print(type(appdaet))
+
+# snake_case (Python)
+# camelCase (JavaScript)
+# PascalCase (C#, Java)
+# kebab-case (URLs)
