b9e88a040c78487928ede1244ed88d129e7b69fc
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-05-29 19:51:55 +0800

    added 05_user_input02.py

---
+++ lecture/Week04/05_user_input02.py
@@ -0,0 +1,10 @@
+adjective = input("adjective: ")
+color = input("color: ")
+animal_1 = input("first animal: ")
+verb = input("verb: ")
+another_adjective = input("another adjective: ")
+animal_2 = input("second animal: ")
+
+print(
+    f"The {adjective} {color} {animal_1} {verb} over the {another_adjective} {animal_2}"
+)
