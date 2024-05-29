2d7422b6c5b2168a61f09852ef4baf6687333223
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-05-29 19:45:06 +0800

    added 04_user_input

---
+++ lecture/Week04/04_user_input.py
@@ -0,0 +1,9 @@
+first_number = input("Give me a number: ")
+second_number = input("Give me another number: ")
+
+print(f"first_number is {first_number}")
+print(type(first_number))
+print(f"second_number is {second_number}")
+print(type(second_number))
+
+print(f"sum of two numbers: {float(first_number) + float(second_number)}")
