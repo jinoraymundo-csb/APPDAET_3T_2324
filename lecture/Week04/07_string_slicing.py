d2aeb49728b93f9d53ba794c823fa8d1b14c467f
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-05-29 20:12:06 +0800

    add 07_string_slicing.py

---
+++ lecture/Week04/07_string_slicing.py
@@ -0,0 +1,14 @@
+school = "De La Salle - College of St. Benilde"
+
+print(school[14:])
+print(school[14:21])
+print(school[14:21:2])
+
+# first number is start
+# second number is stop
+# third number is step/skip
+
+print(school[:11])
+print(school[0:11])
+
+print(school[::-1])
