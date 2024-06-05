39c36a7a10aaca9c635f925230237477648eb40a
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-06-05 18:45:05 +0800

    added 03_string_interpolation.py

---
+++ lecture/Week05/03_string_interpolation.py
@@ -0,0 +1,6 @@
+degree_program = "Information Systems"
+school = "De La Salle - College of St. Benilde"
+campus = "The Atrium"
+
+print(f"I am taking up {degree_program} at {school}.")
+print(f"Most of my classes are held in {campus}")
