9b3725c43489797c91d8297416e5a292287d2437
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-06-05 19:46:05 +0800

    added 07_list_indexing.py

---
+++ lecture/Week05/07_list_indexing.py
@@ -0,0 +1,8 @@
+car_types = ["sedan", "hatchback", "SUV", "van"]
+
+print(car_types[0])
+print(car_types[len(car_types) - 1])  # car_types[3]
+print(car_types[-1])
+
+print(car_types[:2])  # car_types[0:2]
+print(car_types[::2])
