8c73739c49bec451b78de536f0d72ac21b6b23ee
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-06-05 18:35:56 +0800

    added 01_strings.py

---
+++ lecture/Week05/01_strings.py
@@ -0,0 +1,19 @@
+# Strings are sequences of characters, enclosed in single (') or 
+# double (") quotes.
+
+# You can also build a multiline string by enclosing the
+# characters in triple quotes (""") or (''')
+
+# You can use the * operator to repeat strings
+
+# You can use the + operator to concatenate strings
+
+# in the print() function, you can use comma (,) to
+# concatenate strings, but it adds a space between them.
+
+
+print(" APPDAET " * 10)
+print("The quick " + "brown fox")
+
+# print("APPDAET" / 2)
+# print("APPDAET" - 2)
