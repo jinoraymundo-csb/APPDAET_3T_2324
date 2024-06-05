f5e23c8c6f9e988367ab7b03d26799cd982c26be
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-06-05 19:50:54 +0800

    added 08_list_concatenation.py

---
+++ lecture/Week05/08_list_concatenation.py
@@ -0,0 +1,8 @@
+car_types = ["sedan", "hatchback", "SUV", "van"]
+air_vehicle_types = ["chopper", "jet"]
+water_vehicle_types = ["yacht", "speedboat"]
+
+vehicle_types = car_types + air_vehicle_types + water_vehicle_types
+print(vehicle_types)
+
+print(car_types * 2)
