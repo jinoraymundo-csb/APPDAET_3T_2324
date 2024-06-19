d75cf4059bfd989b0fe94a54102e55d9f1867a9c
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-06-19 19:50:03 +0800

    added 07_lists_and_tuples

---
+++ lecture/Week06/07_lists_vs_tuples.py
@@ -0,0 +1,14 @@
+# lists are mutable
+stations = ["Recto", "Legarda", "V.Mapa"]
+stations[0] = "Claro M. Recto"
+print(stations)
+
+# tuples are immutable
+cities = ("Manila", "Mandaluyong", "Pasay")
+# cities[0] = "Maharlika"
+print(cities)
+
+# strings are also immutable
+school = "Benilde"
+# school[1] = "i"
+print(school)
