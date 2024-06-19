0b02579ac49da5270a34f0090faa951fb7bcd5e7
jinoraymundo-csb <jino.raymundo@benilde.edu.ph>
2024-06-19 18:43:22 +0800

    added 02_not_in_operator.py

---
+++ lecture/Week06/02_not_in_operator.py
@@ -0,0 +1,17 @@
+fruits = [
+    "apple",
+    "pomegranate",
+    "kiwi",
+    "salak",
+    "blackberry"
+]
+
+fruit_to_find = "kiwi"
+
+print(f"is {fruit_to_find} not in {fruits}? {fruit_to_find not in fruits}")
+print(f"{fruit_to_find not in fruits}")
+
+superstars = [77, 15, 2, 34, 0]
+print(23 not in superstars)
+print(35 not in superstars)
+print(77 not in superstars)
